ffd4d85e12178d13f02212034330ac9a112caa7c
Yudanova <a2798252y@gmail.com>
2025-08-19 17:21:31 +0300

    Fixture autorization user data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,46 @@
-import pytest  # Импортируем pytest
-from playwright.sync_api import sync_playwright, Page, Playwright
-     # Имопртируем класс страницы, будем использовать его для аннотации типов
+from pathlib import Path
+import re
+import uuid
+import pytest
+from playwright.sync_api import sync_playwright, expect,Page, Playwright
+
+@pytest.fixture(scope="session") # runs once per test session
+def initialize_browser_state(playwright: Playwright): # Register a new user and save the browser state to a file. So you don’t have to repeat registration in every test
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    registration_email_input.fill("user.name@gmail.com")
+
+    registration_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    registration_name_input.fill("username")
+
+    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_password_input.fill("password")
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    context.storage_state(path="browser-state.json") # No return value. It just creates browser-state.json
+
+    browser.close()
 
 
+@pytest.fixture(scope="function") # fresh page for each test
+# Opens a browser page using the saved state from browser-state.json
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+        # Return a Page object you can use in your test, Returns a page with that stat
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+    yield page  # This is where Pytest pauses the fixture and passes control to my test, test runs using the page object returned by yield.
+
+    browser.close() # Once test finishes, Pytest resumes the fixture after the yield, which is where i clean up:
+
+
+# Имопртируем класс страницы, будем использовать его для аннотации типов
 # @pytest.fixture  # Declare fixture, scope function by default (what we need)
 # def chromium_page() -> Page:  # Аннотируем возвращаемое фикстурой значение
 #     # Ниже идет инициализация и открытие новой страницы
@@ -16,8 +54,8 @@ from playwright.sync_api import sync_playwright, Page, Playwright
 #         # Закрываем браузер после выполнения тестов
 #         browser.close()
 
-@pytest.fixture
-def chromium_page(playwright: Playwright) -> Page:
-        browser = playwright.chromium.launch(headless=False)
-        yield browser.new_page()
-        browser.close()
+# @pytest.fixture
+# def chromium_page(playwright: Playwright) -> Page:
+#         browser = playwright.chromium.launch(headless=False)
+#         yield browser.new_page()
+#         browser.close()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,50 +1,62 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect, Page
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(): # Create test function
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state: Page):
+    page = chromium_page_with_state
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        registration_email_input.fill("user.name@gmail.com")
+    #   Title "Courses"
 
-        registration_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        registration_name_input.fill("username")
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
 
-        registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        registration_password_input.fill("password")
+    #  # Empty view icon
 
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
+    empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
 
-        context.storage_state(path="browser-state.json")
+    # Empty view title
 
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")
-        page = context.new_page()
-        # page = browser.new_page()
+    empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-        courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_title).to_be_visible()
-        expect(courses_title).to_have_text('Courses')
+    # Empty view description
 
-        empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(empty_view_icon).to_be_visible()
+    empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
 
-        empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(empty_view_title).to_be_visible()
-        expect(empty_view_title).to_have_text('There is no results')
+# def test_empty_courses_list(): # Create test function
+#     with sync_playwright() as playwright:
+#         browser = playwright.chromium.launch(headless=False)
+#         context = browser.new_context()
+#         page = context.new_page()
 
-        empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(empty_view_description).to_be_visible()
-        expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
+        # page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        # registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        # registration_email_input.fill("user.name@gmail.com")
+        #
+        # registration_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        # registration_name_input.fill("username")
+        #
+        # registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        # registration_password_input.fill("password")
+        #
+        # registration_button = page.get_by_test_id('registration-page-registration-button')
+        # registration_button.click()
+        #
+        # context.storage_state(path="browser-state.json")
+
+    # with sync_playwright() as playwright:
+    #     browser = playwright.chromium.launch(headless=False)
+    #     context = browser.new_context(storage_state="browser-state.json")
+    #     page = context.new_page()
+    #     # page = browser.new_page()
+
 
         # page.wait_for_timeout(5000)
         # python -m pytest -v -s -k "test_empty_courses_list"
