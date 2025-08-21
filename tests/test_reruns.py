ccde7b913f92e1fc17333345468137ac16aa2af7
Yudanova <a2798252y@gmail.com>
2025-08-21 16:26:08 +0300

    rerun autotests

---
+++ tests/test_reruns.py
@@ -0,0 +1,34 @@
+import random
+import pytest
+
+PLATFORM = "Linux" #"Windows"   #"Linux"
+
+
+@pytest.mark.flaky(reruns=3, reruns_delay=2)  # Rerun with marker flaky.
+# Where: reruns=3 — amount of reruns, if test will fail it will be rerun 3 times; reruns_delay=2 - Delay between reruns in seconds
+def test_reruns():
+    assert random.choice([True, False])  # Random choice for unstable test, случайным образом выбирает True или False as a result, что создаёт нестабильное поведение теста.
+    # assert False
+
+
+@pytest.mark.flaky(reruns=3, reruns_delay=2)  # Test class
+class TestReruns:
+    def test_rerun_1(self):
+        assert random.choice([True, False])
+
+    def test_rerun_2(self):
+        assert random.choice([True, False])
+
+
+
+    @pytest.mark.flaky(reruns=3, reruns_delay=2, condition=PLATFORM == "Linux")  # With condition - see cons - "PLATFORM" / Перезапуск при выполнении условия
+    def test_rerun_with_condition():
+        assert random.choice([True, False])
+
+
+
+
+
+# python -m pytest -k "test_reruns" -s -v
+
+
